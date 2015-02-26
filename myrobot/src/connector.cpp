84d6b111e9876c8495656e1c1f65feb32d1e7f34
lazorg64 <lazorg64@gmail.com>
2015-02-26 19:03:40 +0300

    управление моторами по команде

--- myrobot/src/connector.cpp
+++ myrobot/src/connector.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sstream>
+#include <iostream>
+#include <string>
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
  ROS_INFO("I heard: [%s]", msg->data.c_str());
@@ -11,12 +13,13 @@ int main(int argc, char **argv) {
  ros::NodeHandle n;
  ros::Subscriber sub = n.subscribe("from_ard", 1000, chatterCallback);
  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("to_ard", 1000);
- ros::Rate loop_rate(500);
+ ros::Rate loop_rate(10);
  while (ros::ok()) {
 	 
-  std_msgs::String msg;
-  chatter_pub.publish(msg);
-  ros::spin();
+  std_msgs::String output;
+  std::cin >> output.data;
+  chatter_pub.publish(output);
+  ros::spinOnce();
   loop_rate.sleep();
   
  }
--- robot_arduino/robot_arduino.ino
+++ robot_arduino/robot_arduino.ino
@@ -1,28 +1,59 @@
 /*
- * rosserial Publisher Example
- * Prints "hello world!"
+ * rosserial PubSub Example
+ * Prints "hello world!" and toggles led
  */
 
 #include <ros.h>
+#include <AFMotor.h>
 #include <std_msgs/String.h>
+ros::NodeHandle nh;
+AF_DCMotor motor(1);
+char input[15];
+void messageCb( const std_msgs::String& toggle_msg){
+  input[0] = toggle_msg.data[0];
 
-ros::NodeHandle  nh;
-
+}
+ros::Subscriber<std_msgs::String> sub("to_ard", messageCb );
 std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
+ros::Publisher chatter("from_ard", &str_msg);
 
-char hello[13] = "hello world!";
+
+
+
+
 
 void setup()
-{
+{       
+  motor.setSpeed(255);
+  motor.run(RELEASE);
+  memset(input,0,15);
+  input[0] = 'a';
+  pinMode(13, OUTPUT);
   nh.initNode();
   nh.advertise(chatter);
+  nh.subscribe(sub);
 }
 
 void loop()
 {
-  str_msg.data = hello;
+  switch(input[0])
+  {
+    case 'w':
+    motor.run(FORWARD);
+    break;
+    
+    case 's':
+    motor.run(BACKWARD);
+    break;
+    
+    case 'q':
+    motor.run(RELEASE);
+    break;
+    
+  }
+
+  str_msg.data = input;
   chatter.publish( &str_msg );
   nh.spinOnce();
-  delay(1000);
+  delay(50);
 }
